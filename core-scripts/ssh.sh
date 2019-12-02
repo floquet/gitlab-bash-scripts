01c43c15c312a9065d6649308b6227f17cd0bac5
Daniel Topa <dantopa@gmail.com>
2019-12-02 15:40:12 -0500

    git, ssh

--- core-scripts/ssh.sh
+++ core-scripts/ssh.sh
@@ -10,7 +10,7 @@ printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 # Mac
 # cat ~/.ssh/id_rsa.pub | pbcopy
 
-# ssh-keygen -t rsa -C "dantopa@lanl.gov" -b 4096
+# ssh-keygen -t rsa -C "dantopa@gmail.com" -b 4096
 alias resetSSH='echo "ssh-keygen -f ~/.ssh/id_rsa -p"; ssh-keygen -f ~/.ssh/id_rsa -p'
 
 # ihpc:    crypto card

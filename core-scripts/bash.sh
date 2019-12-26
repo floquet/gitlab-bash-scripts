a00a2e642763fa6694d9827e62d345b6f8c29b56
Daniel Topa <daniel.topa@ertcorp.com>
2019-12-25 22:43:22 -0700

    ls -alh --color

--- core-scripts/bash.sh
+++ core-scripts/bash.sh
@@ -5,7 +5,7 @@ printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 export os="$(tr [A-Z] [a-z] <<< "$(uname)")" # operating system, lower case
 
 # #   A L I A S
-alias    lss="ls -alh"
+alias    lss="ls -alh --color"
 alias     sr="source ${HOME}/${bash_file}"  # source bash file
 alias   bcat="cat    ${HOME}/${bash_file}"  # cat    bash file
 alias setnow='export dir_now="$(pwd)"; gonow=cd ${dir_now}'

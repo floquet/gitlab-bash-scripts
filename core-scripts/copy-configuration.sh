e009959dfda0bd7901cfdd4b64f4613dcd156e36
Daniel Topa <daniel.topa@ertcorp.com>
2020-01-12 18:29:13 -0700

    vault -> configuration

--- core-scripts/copy-configuration.sh
+++ core-scripts/copy-configuration.sh
@@ -2,8 +2,8 @@
 printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
 # # C O P Y
-cp ${HOME}/${bash_file} ${vault} &
-cp ${HOME}/.vimrc       ${vault} &
-cp ${HOME}/.gitconfig   ${vault} &
+cp ${HOME}/${bash_file} ${configuration} &
+cp ${HOME}/.vimrc       ${configuration} &
+cp ${HOME}/.gitconfig   ${configuration} &
 
 #wait

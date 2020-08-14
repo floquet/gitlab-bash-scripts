fb359f8e13528df8eea7910b47971bd7a9a5970b
Daniel M. Topa <dantopa@gmail.com>
2020-08-14 09:19:42 -0600

    drop dropbox from quaxolotl mount in docker

--- core-scripts/docker.sh
+++ core-scripts/docker.sh
@@ -4,8 +4,8 @@ printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 alias dimage="echo 'docker images'; docker images"
 
 function quaxolotl(){
-    echo "docker run -it -v /Volumes/Chaac:/Chaac -v /Volumes/Opochtli:/Opochtli -v /Users/dantopa/Dropbox:/Dropbox ${1}"
-          docker run -it -v /Volumes/Chaac:/Chaac -v /Volumes/Opochtli:/Opochtli -v /Users/dantopa/Dropbox:/Dropbox ${1}
+    echo "docker run -it -v /Volumes/Chaac:/Chaac -v /Volumes/Opochtli:/Opochtli ${1}"
+          docker run -it -v /Volumes/Chaac:/Chaac -v /Volumes/Opochtli:/Opochtli ${1}
 }
 
 function xiuhcoatl(){

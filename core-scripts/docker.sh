a21672ff0606d8fdb971f940a536c9524d111f32
Daniel Topa <dantopa@gmail.com>
2020-08-16 01:37:29 -0600

    fix merge problem

--- core-scripts/docker.sh
+++ core-scripts/docker.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
-<<<<<<< HEAD
-export dirDockerLocker="${ddocker}"
-=======
 alias dimage="echo 'docker images'; docker images"
->>>>>>> fb359f8e13528df8eea7910b47971bd7a9a5970b
+export dirDockerLocker="${ddocker}"
 
 function quaxolotl(){
     echo "docker run -it -v /Volumes/Chaac:/Chaac -v /Volumes/Opochtli:/Opochtli ${1}"

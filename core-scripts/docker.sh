ed38b383e7886c00baf78fb104dd80e87e3fbd80
Daniel Topa <dantopa@gmail.com>
2020-08-18 11:55:45 -0600

    alias bye="echo 'lsb_release -a; exit'; lsb_release -a; exit"

--- core-scripts/docker.sh
+++ core-scripts/docker.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
 printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
+alias bye="echo 'lsb_release -a; exit'; lsb_release -a; exit"
 alias dimage="echo 'docker images'; docker images"
+
 export dirDockerLocker="${ddocker}"
 
 function quaxolotl(){
     echo "docker run -it -v /Volumes/Chaac:/Chaac -v /Volumes/Opochtli:/Opochtli ${1}"
           docker run -it -v /Volumes/Chaac:/Chaac -v /Volumes/Opochtli:/Opochtli ${1}
+}
+
+function quaxolotlx(){
+    echo "docker run -it -v /Volumes/Chaac:/Chaac ${1}"
+          docker run -it -v /Volumes/Chaac:/Chaac ${1}
 }
 
 function xiuhcoatl(){

5aa9921bbce55a5521e785e7b6d35ad17d938e7c
Daniel Topa <dantopa@gmail.com>
2021-10-30 11:17:14 -0600

    copy-configuration.sh

--- core-scripts/copy-configuration.sh
+++ core-scripts/copy-configuration.sh
@@ -2,6 +2,7 @@
 printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
 mkdir -p ${configuration}
+echo "copying configuration files to \${configuration} = ${configuration}"
 
 # # C O P Y
 cp ${HOME}/${bash_file} ${configuration} &

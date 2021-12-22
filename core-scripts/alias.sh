4e6344fee959604ccb2e93b72bc555f5309b2692
Daniel M. Topa <dantopa@gmail.com>
2021-12-22 15:30:09 -0700

    Epic of dual outputs

--- core-scripts/alias.sh
+++ core-scripts/alias.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
-export my_log="${configuration}/alias-assignments.txt"
+function alias_tasker(){
+    write_alias_assignments ${configuration}/${1}
+    write_alias_assignments        ${locker}/${1}
+}
+function write_alias_assignments (){
+    echo "alias assignments on ${moniker}" >  ${1}
+    date                                   >> ${1}
+    echo ""                                >> ${1}
+    alias                                  >> ${1}
+}
 
-echo "alias assignments on ${host_name}" >  ${my_log}
-date                                     >> ${my_log}
-
-echo ""                                  >> ${my_log}
-alias                                    >> ${my_log}
+alias_tasker alias-assignments.txt
 
 alias contents_alias='echo "write list of alias commands to \${my_log} = ${my_log}"'
--- core-scripts/info-git.sh
+++ core-scripts/info-git.sh
@@ -4,7 +4,7 @@ printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 export dirHere=$(pwd)
 # git pull
 git add -A .
-git commit -m "${host_name}-${partition}-${node_name} $(date)"
+git commit -m "${moniker}-${dist}-${release} $(date)"
 # git push
 
 cd ${dirHere}

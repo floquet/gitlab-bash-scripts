5c30e82064a97133fe4868fe8ef98724ceb775de
Daniel Topa <dantopa@gmail.com>
2022-01-19 18:25:25 -0700

    Update copy-configuration.sh

--- core-scripts/copy-configuration.sh
+++ core-scripts/copy-configuration.sh
@@ -5,13 +5,12 @@ printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 # ${config_repo}: /Volumes/repos/bitbucket/mac-configurations
 # ${dir_config}: Macmini8,1-(xiuhcoatl)/darwin 21.2.0/Monterey-12.1
 export dir_inits=${config_repo}/${dir_config}
-mkdir -p ${dir_inits}/.info
+mkdir -p ${dir_inits}
 
 # # C O P Y
 rsync -am ${HOME}/*.sh      ${dir_inits}       &
 rsync -am ${HOME}/.git*     ${dir_inits}       &
 rsync -am ${HOME}/.vim*     ${dir_inits}       &
-rsync -am ${HOME}/.zprofile ${dir_inits}       &
 rsync -am ${HOME}/.info/.   ${dir_inits}/.info &
 
 wait

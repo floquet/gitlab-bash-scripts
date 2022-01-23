fb03e4ddd8f0e9cfc15e96f906f7cf52e31ee8e8
Daniel M. Topa <dantopa@gmail.com>
2022-01-22 18:35:53 -0700

    Update prompt.sh

--- core-scripts/prompt.sh
+++ core-scripts/prompt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-printf '%s\n' "$(tput bold)$(date) ${BASH_SOURCE[0]}$(tput sgr0)"
+printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
 ##  ##  set environment
 # http://unix.stackexchange.com/questions/177572/how-to-rename-terminal-tab-title-in-gnome-terminal

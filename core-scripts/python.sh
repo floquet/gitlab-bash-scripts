0ba2ed5d3fe71d1528d5907c1d8a2a417efb5c1c
Daniel Topa <dantopa@gmail.com>
2022-01-31 17:08:43 -0700

    Update python.sh

--- core-scripts/python.sh
+++ core-scripts/python.sh
@@ -3,3 +3,7 @@ printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
 export dirPyVirtualEnv="${HOME}/scratch/python-virtual-environments"
 mkdir -p ${dirPyVirtualEnv}
+
+export dirPyTruth="${dirPyVirtualEnv}/truth"
+
+

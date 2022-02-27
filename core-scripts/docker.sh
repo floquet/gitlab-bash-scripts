451637d50a62f27b3a97fdb54a2987d593ed89d5
Daniel Topa <dantopa@gmail.com>
2022-02-26 19:26:24 -0700

    Update docker.sh

--- core-scripts/docker.sh
+++ core-scripts/docker.sh
@@ -9,16 +9,21 @@ alias dimage="echo 'docker images'; docker images"
 
 export centos_version="7.9.2009"
 export ubuntu_version="22.04"
+export amazon_version="2022.0.20220202.0"
+
 export dirDockerLocker="${ddocker}"
 export dirDropbox="${repo}/spacktivity/mirror"
 export file_docker_log="${repo_results_docker}/vm-log-book.txt"
 alias mirrorBigSpackMirror="echo 'spack mirror add local_filesystem file://${big_spack_mirror}';spack mirror add local_filesystem file://${big_spack_mirror}"
+
+# docker container prune
 
 #alias reap="${rgaddr}/repos/bitbucket/spack_tools/scripts/reaper.bash"
 #alias reap="echo '${rgaddr}/repos/bitbucket/spack_tools/scripts/reaper.bash; git -C ${stools} commit -m ''test'''; ${rgaddr}/repos/bitbucket/spack_tools/scripts/reaper.bash; git -C ${stools} commit -m ''test''"
 #export generics="${ddocker}/unified/generics/"
 
 function xiuhcoatlDocker(){
+    docker_logger
 # myDocker ubuntu:22.04
     echo "docker run -it -v /Users/dantopa/Dropbox:/Dropbox -v /Volumes/Metztli:/Metztli -v /Volumes/Infernum:/Infernum -v /Volumes/Paradisum:/Paradisum -v /Volumes/Purgatorium:/Purgatorium -v /Volumes/atacama:/atacama -v /Volumes/gobi:/gobi -v /Volumes/sonoran:/sonoran -v /Volumes/repos:/repos -v /Volumes/spacktivity:/spacktivity ${1}"
     docker run -it \
@@ -35,6 +40,7 @@ function xiuhcoatlDocker(){
 }
 
 function xiuhcoatlDockerTime {
+    docker_logger
 # myDocker ubuntu:22.04
     echo "docker run -it -v /etc/localtime:/etc/localtime -v /Users/dantopa/Dropbox:/Dropbox -v /Volumes/Metztli:/Metztli -v /Volumes/Infernum:/Infernum -v /Volumes/Paradisum:/Paradisum -v /Volumes/Purgatorium:/Purgatorium -v /Volumes/atacama:/atacama -v /Volumes/gobi:/gobi -v /Volumes/sonoran:/sonoran -v /Volumes/repos:/repos -v /Volumes/spacktivity:/spacktivity ${1}"
     docker run -it                     \
@@ -52,6 +58,7 @@ function xiuhcoatlDockerTime {
 }
 
 function quaxolotlDocker(){
+    docker_logger
 # volume_ext set in .quaxolotl.sh as /Volumes/T7-Touch
     echo "docker run -it -v ${volume_ext}/Dropbox:/Dropbox -v ${volume_ext}/repos:/repos -v ${volume_ext}/spacktivity:/spacktivity ${1}"
     docker run -it \
@@ -61,6 +68,7 @@ function quaxolotlDocker(){
 }
 
 function quaxolotlDockerTime(){
+    docker_logger
 # volume_ext set in .quaxolotl.sh as /Volumes/T7-Touch
     echo "docker run -it -v /etc/localtime:/etc/localtime -v ${volume_ext}/Dropbox:/Dropbox -v ${volume_ext}/repos:/repos -v ${volume_ext}/spacktivity:/spacktivity ${1}"
     docker run -it \
@@ -71,6 +79,7 @@ function quaxolotlDockerTime(){
 }
 
 function ehecoatlDockerTime(){
+    docker_logger
 # volume_ext set in .quaxolotl.sh as /Volumes/T7-Touch
     echo "docker run -it -v /etc/localtime:/etc/localtime -v ${HOME}/Dropbox:/Dropbox -v ${volume_ext}/repos:/repos -v ${volume_ext}/spacktivity:/spacktivity ${1}"
     docker run -it \
@@ -83,6 +92,7 @@ function ehecoatlDockerTime(){
 }
 
 function ehecoatlDockerTimeGitlab(){
+    docker_logger
 # volume_ext set in .quaxolotl.sh as /Volumes/T7-Touch
     echo "docker run -it -v /etc/localtime:/etc/localtime -v ${HOME}/Dropbox:/Dropbox -v ${volume_ext}/repos:/repos -v ${volume_ext}/spacktivity:/spacktivity ${1}"
     docker run -it \
@@ -96,6 +106,7 @@ function ehecoatlDockerTimeGitlab(){
 }
 
 function ehecoatlDocker(){
+    docker_logger
 # volume_ext set in .quaxolotl.sh as /Volumes/T7-Touch
     echo "docker run -it -v ${HOME}/Dropbox:/Dropbox -v ${HOME}/repos:/repos -v ${volume_ext}/repos:/vrepos -v ${volume_ext}/spacktivity:/spacktivity ${1}"
     docker run -it \
@@ -104,8 +115,11 @@ function ehecoatlDocker(){
  -v ${volume_ext}/repos:/vrepos \
  -v ${volume_ext}/spacktivity:/spacktivity  ${1}
 }
+
+function docker_logger{
 # docker_logger
 if [[ ${owner} == "docker" ]]; then
     # echo "" >> ${file_docker_log}
     echo "$(date +%Y-%m-%d\ %H:%M:%S) ${dist}-${release}, network node hostname = $(uname -n), ${machine}-(${moniker})"  >> ${file_docker_log}
 fi
+}
